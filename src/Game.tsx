1e45a68b03bf9afd9b90faa85dba96699a6a7293
louisevelayo <louisematiasivsegismundo.velayo@student.kuleuven.be>
2023-06-17 21:10:46 +0200

    clean up

---
+++ src/Game.tsx
@@ -0,0 +1,16 @@
+function Game() {
+  return (
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">
+        Tic Tac Toe Game Page
+      </h1>
+      <div>
+        <div>Board Goes Here</div>
+
+        <div>Score Goes Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
