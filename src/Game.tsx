b239744d6c8f6120bbce14b7cd5c0b15c1557797
louisevelayo <louisematiasivsegismundo.velayo@student.kuleuven.be>
2023-06-19 15:35:01 +0200

    cell click indicates which cell is clicked

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import Square from "./Square";
+
+const INITIAL_GAME_STATE = ["X", "O", "", "", "", "", "", "", ""];
+
+const handleCellClick = (event: any) => {
+  console.log("cell clicked!", event.target.getAttribute("data-cell-index"))
+}
+
 function Game() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">
         Tic Tac Toe Game Page
       </h1>
       <div>
-        <div>Board Goes Here</div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {gameState.map((player, index) => (
+            <Square key={index} onClick={handleCellClick} {...{index, player}}/>
+          ))}
+        </div>
 
         <div>Score Goes Here</div>
       </div>
